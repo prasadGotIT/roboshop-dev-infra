d86eda13a6178e74447ac7f0f2cd74eea966440d
prasad sripathi <prasad18218@gmail.com>
2026-01-13 21:31:59 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -1,7 +1,9 @@
 resource "aws_security_group_rule" "backend_alb_bastion" {
+    type = "ingress"
+    
     security_group_id = local.backend_alb_sg_id
     source_security_group_id = local.bastion_sg_id
-    type = "ingress"
+    
     from_port = 80
     protocol = "tcp"
     to_port = 80
