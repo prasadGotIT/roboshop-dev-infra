fa8137c3536202d8a3592369ccb59a5d6a78c765
prasad sripathi <prasad18218@gmail.com>
2026-01-14 00:24:46 +0530

    terraform

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,9 +1,7 @@
 module "vpc" {
-    #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/prasadGotIT/terraform-aws-vpc.git"
-    /* vpc_cidr = "10.0.0.0/16"
-    project_name = "roboshop"
-    environment = "dev" */
+    
+    source = "git::https://github.com/prasadGotIT/terraform-aws-vpc.git?ref=main"
+    
 
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
