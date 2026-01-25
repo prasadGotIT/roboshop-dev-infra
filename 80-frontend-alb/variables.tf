294e04597c68bcd8006fa800cc22af56fc254f88
prasad sripathi <prasad18218@gmail.com>
2026-01-25 11:53:17 +0530

    terraform

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "domain_name" {
+    default = "spdevhops.shop"
+  
+}
+
+variable "zone_id" {
+    default = "Z015504635YTPJNLLKES7"
+  
+}
