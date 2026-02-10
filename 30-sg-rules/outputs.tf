1e24617ffdafdec500e903ad7eaf9fec02c145c6
prasad sripathi <prasad18218@gmail.com>
2026-02-10 12:55:11 +0530

    terraform

---
+++ 30-sg-rules/outputs.tf
@@ -0,0 +1,5 @@
+# output "shipping_bastion" {
+#     value = aws_security_group_rule.shipping_bastion
+#     sensitive = fasks
+  
+# }
--- 90-components/main.tf
+++ 90-components/main.tf
@@ -9,6 +9,5 @@ module "components" {
     for_each = var.components
     source = "git::https://github.com/prasadGotIT/terraform-roboshop-component.git?ref=main"
     component = each.key
-    rule_priority = each.value.rule_priority
-  
+    rule_priority = each.value.rule_priority 
 }
